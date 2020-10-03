eacb453ab4dba40c842376b9bbd77f1e12b353ca
Dawn <alek.kuntsevich@gmail.com>
2020-10-03 14:23:56 +0300

    hom_work2process

--- src/HomeWork2/Arrays2_2.java
+++ src/HomeWork2/Arrays2_2.java
@@ -1,23 +1,25 @@
 package HomeWork2;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Arrays2_2 {
     public static void main(String[] args) {
-        int[] arr = new int[]{8, 8, 0, 0, 5, 5, 5, 3, 5, 3, 5};
-        int[] arr2 = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Scanner IN = new Scanner(System.in);
+        System.out.print("Размер массива: ");
+        int size = IN.nextInt();
+        System.out.println();
+        int[] arr = new int[size];
+        int c = 0;
+        for (int number:arr) {
+                arr[c] = IN.nextInt();
+                System.out.println("Индекс в массиве: " + c++);
+        }
         int b;
         for (int i = 0; i <= arr.length - 1; i++) {
             b = i;
             if ((b %= 2) == 0){
                 System.out.print(arr[i] + " ");
-            }
-        }
-        System.out.println("\n");
-        for (int i = 0; i <= arr2.length - 1; i++) {
-            b = i;
-            if ((b %= 2) == 0){
-                System.out.print(arr2[i] + " ");
             }
         }
     }
--- src/HomeWork2/Arrays2_3.java
+++ src/HomeWork2/Arrays2_3.java
@@ -4,11 +4,37 @@ import java.util.Scanner;
 
 public class Arrays2_3 {
     public static void main(String[] args) {
-        Scanner consol = new Scanner(System.in);
-        System.out.print("Элементы массива : ");
-        int consolArray = consol.nextInt();
-        int[] arr = {consolArray};
-        System.out.println(consolArray);
+        Scanner IN = new Scanner(System.in);
+        System.out.print("Размер массива: ");
+        int size = IN.nextInt();
+        System.out.println();
+        int[] arr = new int[size];
+        int c = 0;
+        for (int number:arr) {   // как-то можно улчучшить
+            arr[c] = IN.nextInt();
+            System.out.println("Индекс в массиве: " + c++);
+        }
+        int i = arr.length - 1 ;
+        System.out.println("do....while");
+        do{
+            System.out.print(arr[i--] + " ");
+            if (i == -1) break;
+        }while (i < arr.length);
+//        System.out.println("\n\nwhile");
+//        i = 0;
+//        while(i < arr.length){
+//            System.out.print(arr[i++] + " ");
+//        }
+//        System.out.println("\n\nfor");
+//        for (int j = 0; j < arr.length; j++) {
+//            System.out.print(arr[j] + " ");
+//        }
+//        System.out.println("\n\nforeach");
+//        for (int print: arr) {
+//            System.out.print(print + " ");
+//        }
+
+
 
 
 
