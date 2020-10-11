dd3bf0fd59cb5086d3ea4453f4d297d7503409b3
Dawn <alek.kuntsevich@gmail.com>
2020-10-11 23:05:06 +0300

    HomeWork3_2task

---
+++ src/HomeWork3/CalculatorWithOperator.java
@@ -0,0 +1,120 @@
+package HomeWork3;
+
+public class CalculatorWithOperator {
+    public static void main(String[] args) {
+        CalculatorWithOperator start = new CalculatorWithOperator();
+        start.multiply(15,7);
+        start.division(28,5);
+        start.exponentiation(start.divEnd,2);
+        start.addition(4.1,start.multipEnd);
+        start.addition(start.sumEnd, start.exponentEnd);
+        System.out.println(start.sumEnd);
+
+
+
+
+
+
+        }
+
+
+    double divEnd;
+    int multipEnd;
+    double subtractEnd;
+    double sumEnd;
+
+
+    double exponentEnd;
+    double modulEnd;
+    double rootEnd;
+
+
+
+
+
+
+    double division(double dividend, double divider) { //метод деления
+        divEnd = dividend / divider;
+        return divEnd;
+    }
+
+    int multiply(int multiplierFirst, int multiplierSecond){   //метод умножения
+        multipEnd = multiplierFirst * multiplierSecond;
+        return multipEnd;
+    }
+
+
+    double subtraction(double minusFirst,double minusSecond){  //метод вычитания
+        subtractEnd = minusFirst - minusSecond;
+        return subtractEnd;
+    }
+
+
+    double addition(double addFirst,double addSecond){ // метод сложения
+        sumEnd = addFirst + addSecond;
+        return sumEnd;
+    }
+
+
+    double exponentiation(double number, int rate){ // метод степени
+        double exp = number;
+        if (rate == 0) exp = 1;
+        else if (rate == 1) exp = number;
+        else{
+            for (int i = 2; i <= rate ; i++) {
+                exp *= number;
+            }
+        }
+        return this.exponentEnd = exp;
+    }
+    
+
+    double modulation(double number){ // метод модуля
+        double mod;
+        if (number > 0){
+            mod = number;
+        }else if (number < 0){
+            mod = -number;
+        }else mod = number;
+        return this.modulEnd = mod;
+    }
+
+
+
+    double rootOFnumber(double number, int rate){   // метод корня ---можно написать собственное возведение в степень но это долго
+        return this.rootEnd = Math.pow(number, 1.0 / rate);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
---
+++ src/HomeWork3/WithOutCalculatorMain.java
@@ -0,0 +1,9 @@
+package HomeWork3;
+
+public class WithOutCalculatorMain {
+    public static void main(String[] args) {
+        double grade = Math.pow((double) 28/5, 2);
+        double sum = 4.1 + 15 * 7 + grade;
+        System.out.println(sum);
+    }
+}
